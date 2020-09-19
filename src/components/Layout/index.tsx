d77b6e9097d467e51c13d5f7aea64b450ac6a67b
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-19 19:14:15 -0300

    Skeleton from shimmer animation created + Loading profile panel component created

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MobileHeader from '../MobileHeader';
 import DesktopHeader from '../DesktopHeader';
@@ -10,6 +10,14 @@ import AdBanner from '../AdBanner';
 import { Container } from './styles';
 
 const Layout: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      /* setIsLoading(false); */
+    }, 1000)
+  }, [])
+
   return (
     <Container>
       <MobileHeader />
@@ -22,9 +30,9 @@ const Layout: React.FC = () => {
 
       <main>
 
-        <LeftColumn />
-        <MiddleColumn />
-        <RightColumn />
+        <LeftColumn isLoading={isLoading} />
+        <MiddleColumn isLoading={isLoading} />
+        <RightColumn isLoading={isLoading} />
 
       </main>
     </Container>
